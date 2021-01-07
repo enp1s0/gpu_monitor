bd9ea3af7d0d72cd527d02c6df4f115dfb21c3a9
mutsuki <mutsuki@momo86.net>
2021-01-07 17:50:55 +0900

    Add parsing gpu ids

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include <ctime>
@@ -19,22 +20,40 @@
 #include "gpu_logger_hip.hpp"
 #endif
 
-void parse_params(unsigned &time_interval, std::string& output_file_name, int& run_command_head, int argc, char** argv) {
+// e.g. Input `str` is "0,1,3", then return vector will be `{0, 1, 3}`.
+std::vector<unsigned> get_gpu_ids(const std::string str) {
+	std::stringstream ss(str);
+	std::string buffer;
+	std::vector<unsigned> result;
+	while (std::getline(ss, buffer, ',')) {
+		result.push_back(std::stoul(buffer));
+	}
+	return result;
+}
+
+void parse_params(unsigned &time_interval, std::string& output_file_name, std::vector<unsigned>& gpu_ids, int& run_command_head, int argc, char** argv) {
 	run_command_head = 1;
 	output_file_name = "gpu.csv";
 	time_interval = 1;
+	gpu_ids = std::vector<unsigned>{0};
 	for (int i = 1; i < argc;) {
 		if (std::string(argv[i]) == "-i") {
 			if (i + 1 >= argc) {
-				throw std::runtime_error("The value of `-i` is not provided");
+				throw std::runtime_error("The value of `-i` was not provided");
 			}
 			time_interval = std::stoul(argv[i+1]);
 			i += 2;
 		} else if (std::string(argv[i]) == "-o") {
 			if (i + 1 >= argc) {
-				throw std::runtime_error("The value of `-o` is not provided");
+				throw std::runtime_error("The value of `-o` was not provided");
 			}
 			output_file_name = argv[i+1];
+			i += 2;
+		} else if (std::string(argv[i]) == "-g") {
+			if (i + 1 >= argc) {
+				throw std::runtime_error("The value of `-g` was not provided");
+			}
+			gpu_ids = get_gpu_ids(argv[i+1]);
 			i += 2;
 		} else if (std::string(argv[i]) == "-h") {
 			time_interval = 0; // This means that this execution is invalid and exits with printing help messages.
@@ -49,7 +68,7 @@ void print_help_message(const char* const program_name) {
 	std::printf("/*** GPU Logger ***/\n");
 	std::printf("\n");
 	std::printf("// Usage\n");
-	std::printf("%s [-i interval(ms){default=1}] [-o output_file_name{default=gpu.csv}] target_command\n", program_name);
+	std::printf("%s [-i interval(ms){default=1}] [-o output_file_name{default=gpu.csv}] [-g gpu_id{default=0}] target_command\n", program_name);
 }
 
 namespace process {
@@ -60,9 +79,10 @@ constexpr char end     = 'E';
 int main(int argc, char** argv) {
 	std::string output_file_name;
 	unsigned time_interval;
+	std::vector<unsigned> gpu_ids;
 	int run_command_head;
 
-	parse_params(time_interval, output_file_name, run_command_head, argc, argv);
+	parse_params(time_interval, output_file_name, gpu_ids, run_command_head, argc, argv);
 
 	if (time_interval < 1 || argc <= 1) {
 		print_help_message(argv[0]);
@@ -86,9 +106,22 @@ int main(int argc, char** argv) {
 		std::ofstream ofs(output_file_name);
 		const auto num_devices = gpu_logger.get_num_devices();
 
+		// Validate given gpu ids
+		bool invalid_gpu_ids = false;
+		for (const auto gpu_id : gpu_ids) {
+			if (gpu_id >= num_devices) {
+				std::fprintf(stderr, "[ERROR] GPU %u is not found\n", gpu_id);
+				invalid_gpu_ids = true;
+			}
+		}
+		if (invalid_gpu_ids) {
+			gpu_logger.shutdown();
+			exit(1);
+		}
+
 		// Output csv header
 		ofs << "index,date,elapsed_time,";
-		for (unsigned gpu_id = 0; gpu_id < num_devices; gpu_id++) {
+		for (const auto gpu_id : gpu_ids) {
 			ofs << "gpu" << gpu_id << "_temp,";
 			ofs << "gpu" << gpu_id << "_power,";
 			ofs << "gpu" << gpu_id << "_memory_used,";
@@ -105,7 +138,7 @@ int main(int argc, char** argv) {
 				<< std::time(nullptr) << ",";
 			const auto end_clock = std::chrono::high_resolution_clock::now();
 			ofs << std::chrono::duration_cast<std::chrono::microseconds>(end_clock - start_clock).count() << ",";
-			for (unsigned gpu_id = 0; gpu_id < num_devices; gpu_id++) {
+			for (const auto gpu_id : gpu_ids) {
 				ofs << gpu_logger.get_current_temperature(gpu_id) << ","
 					<< gpu_logger.get_current_power(gpu_id) << ","
 					<< gpu_logger.get_current_used_memory(gpu_id) << ",";
