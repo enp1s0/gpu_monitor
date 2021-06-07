3c3bceb7e1713b0310f44800a843336e265a4252
mutsuki <mutsuki@momo86.net>
2021-06-07 12:16:36 +0900

    Add test for `insert_message`

---
+++ test/message_insert_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <gpu_monitor/gpu_monitor.h>
+#include <unistd.h>
+
+int main() {
+	const auto message_file_path = getenv(mtk::gpu_monitor::message_file_path_env_name);
+	std::printf("message_file_path = %s\n", message_file_path);
+
+	mtk::gpu_monitor::insert_message("hello");
+
+	sleep(5);
+}
